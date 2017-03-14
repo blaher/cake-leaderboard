3a069cb588794558ba32ab089538acf2604432cb
Benjamin J. Young <blaher09@gmail.com>
2017-03-13 21:54:19 -0400

    Forgot config structure change

--- config.example.php
+++ config.example.php
@@ -2,5 +2,5 @@
   $config = array(
     'host' => 'www.mycakesite.com',
     'api_key' => 'XjvdeijpYd5FRoNmRMeQ',
-    'affiliate_ids' => array('13', '29', '42')
+    'affiliate_ids' => array('13'=>'Bob Kelso', '29'=>'Turk Turkleton', '42'=>'JD')
   );
