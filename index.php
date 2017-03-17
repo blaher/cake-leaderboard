e8c49865a4893b931886e19e6facd598f6552cc9
Benjamin J. Young <blaher09@gmail.com>
2017-03-16 21:36:34 -0400

    Adding Daily

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+  date_default_timezone_set('America/New_York');
   require_once('config.php');
 
   $endpoint = $config['host'].'/affiliates/api/5/reports.asmx/CampaignSummary';
@@ -6,6 +7,38 @@
   $weekly_affiliates = array();
   $monthly_affiliates = array();
   $yearly_affiliates = array();
+
+  // Daily
+  foreach ($config['affiliate_ids'] as $affiliate_id=>$array) {
+    $params = array(
+      'api_key' => $array['api_key'],
+      'affiliate_id' => $affiliate_id,
+      'start_date' => date('m/d/Y 00:00:00'),
+      'end_date' => date('m/d/Y 00:00:00', strtotime('+1 day')),
+      'sub_affiliate' => '',
+      'conversion_type' => 'all',
+      'start_at_row' => 1,
+      'row_limit' => 1,
+      'sort_field' => 'offer_id',
+      'sort_descending' => 'FALSE'
+    );
+    $params = json_encode($params);
+
+    $ch = curl_init($endpoint);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
+      'Content-Length: '.strlen($params)
+    ));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $data = json_decode(curl_exec($ch));
+    curl_close($ch);
+
+    $daily_affiliates[$array['name']] = $data->d->summary->revenue;
+  }
 
   // Weekly
   $day = date('w');
@@ -104,12 +137,20 @@
     $yearly_affiliates[$array['name']] = $data->d->summary->revenue;
   }
 
+  arsort($daily_affiliates);
   arsort($weekly_affiliates);
   arsort($monthly_affiliates);
   arsort($yearly_affiliates);
 ?>
 
 <html><body>
+
+<h1>Daily</h1>
+<ol>
+  <?php foreach ($daily_affiliates as $name=>$amount): ?>
+    <li><strong><?php echo $name; ?></strong> - $<?php echo number_format($amount); ?></li>
+  <?php endforeach; ?>
+</ol>
 
 <h1>Weekly</h1>
 <ol>
