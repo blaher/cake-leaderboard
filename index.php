e70fda2155f2ca2f369a1121bd5579b86b572f88
Benjamin J. Young <blaher09@gmail.com>
2017-03-16 20:39:20 -0400

    Adding weekly

--- index.php
+++ index.php
@@ -3,8 +3,42 @@
 
   $endpoint = $config['host'].'/affiliates/api/5/reports.asmx/CampaignSummary';
 
+  $weekly_affiliates = array();
   $monthly_affiliates = array();
   $yearly_affiliates = array();
+
+  // Weekly
+  $day = date('w');
+  foreach ($config['affiliate_ids'] as $affiliate_id=>$array) {
+    $params = array(
+      'api_key' => $array['api_key'],
+      'affiliate_id' => $affiliate_id,
+      'start_date' => date('m/d/Y 00:00:00', strtotime('-'.$day.' days')),
+      'end_date' => date('m/d/Y 00:00:00', strtotime('+1 month')),
+      'sub_affiliate' => '',
+      'conversion_type' => 'all',
+      'start_at_row' => 1,
+      'row_limit' => 1,
+      'sort_field' => 'offer_id',
+      'sort_descending' => 'FALSE'
+    );
+    $params = json_encode($params);
+
+    $ch = curl_init($endpoint);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
+      'Content-Length: '.strlen($params)
+    ));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $data = json_decode(curl_exec($ch));
+    curl_close($ch);
+
+    $weekly_affiliates[$array['name']] = $data->d->summary->revenue;
+  }
 
   // Monthly
   foreach ($config['affiliate_ids'] as $affiliate_id=>$array) {
@@ -75,6 +109,13 @@
 ?>
 
 <html><body>
+
+<h1>Weekly</h1>
+<ol>
+  <?php foreach ($weekly_affiliates as $name=>$amount): ?>
+    <li><?php echo $name; ?></li>
+  <?php endforeach; ?>
+</ol>
 
 <h1>Monthly</h1>
 <ol>
